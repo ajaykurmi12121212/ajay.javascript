5408b908be0461a825fb6e88147401ed72c31f26
ajaykurmi12121212 <141719545+ajaykurmi12121212@users.noreply.github.com>
2024-07-22 14:21:18 +0000

    datatype

---
+++ basic/1_variable.js
@@ -0,0 +1,12 @@
+const accountId =144553
+let accountEmail = "ajaykurmi681@gmail.com"
+var accountPassword ="12345"
+accountCity ="bareilly"
+accountEmail="acb@gmail.com"
+accountPassword=23
+accountCity="pilibhit"
+let accountState
+
+// accountId=2
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ basic/2_datatype.js
@@ -0,0 +1,20 @@
+"use strict";// treat all js code as newer version
+//alert(3 + 3) we are using not browser
+console.log(3
+    +3 // code readibility should be high 
+
+)
+let name ="ajay"
+let age =4
+let isLoggedIn=true 
+
+// number => 2 to power 53
+// bigint
+//string=""
+//boolean=true/false
+//null 
+// undefined
+//symbol
+
+//object
+ console.log(typeof null)
