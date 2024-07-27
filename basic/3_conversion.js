d551c149dbd047ba56901a66ec42d4875750da91
ajaykurmi12121212 <141719545+ajaykurmi12121212@users.noreply.github.com>
2024-07-27 16:54:42 +0000

    datatype

---
+++ basic/3_conversion.js
@@ -0,0 +1,31 @@
+let score ="33"
+console.log(typeof score)
+console.log(typeof (score));
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+console.log(typeof valueInNumber );
+
+// *****************operation***********
+let value =3
+let negvalue=-value
+console.log(negvalue);
+console.log(3+3);
+console.log(3-2);
+console.log(3*3);
+console.log(3**3);
+console.log(3/3);
+console.log(3%3);
+ 
+let str1="Ajay"
+let str2 =" kumar"
+
+console.log(str1+str2);
+console.log("3"+1);
+console.log(3+"1");
+console.log("3"+5+4);
+console.log(3+4+"5");
+console.log(+true);
+console.log(+"");
+ let gameCounter=100
+ ++gameCounter
+ console.log(gameCounter);
