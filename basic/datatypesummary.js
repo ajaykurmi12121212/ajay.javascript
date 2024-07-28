57f247cfc7e641e5f184c999aa504f1ad4482f95
ajaykurmi12121212 <141719545+ajaykurmi12121212@users.noreply.github.com>
2024-07-28 14:13:56 +0000

    datatype summary

---
+++ basic/datatypesummary.js
@@ -0,0 +1,26 @@
+/* primitive
+string
+Number
+boolean
+null
+undifined
+symbol
+BigInt*/
+
+
+ 
+/*Nonprimitive (reference type)*/
+//array
+const heros=["ravi","mohit","narendra"]
+
+
+//object
+let myobj={
+    name:"ajay",
+    age:23,
+}
+//function
+const myFunction = function(){
+    console.log("hello wirld");
+}
+console.log(typeof myobj);
