1173868ec2c4cd83726f7a7e735eb55b296b11ce
ajaykurmi12121212 <141719545+ajaykurmi12121212@users.noreply.github.com>
2024-08-05 13:32:34 +0000

    time and date

---
+++ basic/date_time.js
@@ -0,0 +1,18 @@
+let myDate= new Date()
+//console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+let createDate=new Date(2024,7,3,12,3)
+console.log(createDate);
+let TimeStamp= Date.now()
+console.log(TimeStamp);
+console.log(createDate.getTime());
+
+
+
+
+
+
