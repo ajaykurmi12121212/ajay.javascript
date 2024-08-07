319aa8df01e12eadbb29d81d5e10fd36289d2e5a
ajaykurmi12121212 <141719545+ajaykurmi12121212@users.noreply.github.com>
2024-08-07 20:10:42 +0000

    array

---
+++ basic/array.js
@@ -0,0 +1,35 @@
+// array
+
+// const myArr = [1,23,5,3,8,5]
+
+// const myaar= new Array(1,8,7,5,)
+// console.log(myArr[0]);
+
+// myArr.push(10)
+// console.log(myArr);
+// myArr.pop(12)
+// console.log(myArr);
+// myArr.unshift(13)
+// console.log(myArr);
+// console.log(myArr.includes(5));
+// console.log(myArr.indexOf(5));
+
+  // slice and splice 
+  const arr1=["b","c","d"]
+  const arr2=["e","f","d"]
+  //arr1.push(arr2)
+  //console.log(arr1);
+   const arr3=arr1.concat(arr2)
+  //console.log(arr3);
+  const arr4=[...arr1,...arr2]
+  console.log(arr4);
+  const ajay=[1,3,4,[5,6],7,[6,7,[8,9,2]]]
+  const real=ajay.flat(Infinity)
+  console.log(real);
+  
+  
+  
+  
+
+
+
--- basic/string.js
+++ basic/string.js
@@ -1,9 +1,8 @@
 const gameName =new String('ajay')
 
-/*console.log(gameName[0]);
+console.log(gameName[0]);
 console.log(gameName.length);
 console.log(gameName.toUpperCase);
 console.log(gameName.__proto__);
 console.log(gameName.charAt(2));
-console.log(gameName.indexOf(a));*/
-
+console.log(gameName.indexOf(a));
